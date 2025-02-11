35a415ad5dd6739a4aea74346f74c0ec0c9cb15d
Astryd Park <artreadcode@gmail.com>
2025-02-11 16:50:38 +0000

    Working with Java Classes

--- QuantumSketchbook/src/Property.java
+++ QuantumSketchbook/src/Property.java
@@ -3,8 +3,52 @@ import java.awt.event.*;
 
 public class Property {
 	String label;
+	Object value;
+	Object defaultValue;
+	String type;
+	Rectangle rect;
+	Font font = new Font("Arial", Font.PLAIN, 24);
 	
-	public Property() {
-		
+	public Property(String label, Object value, String type) {
+		this.label = label;
+		this.value = value;
+		this.type = type;
+		this.defaultValue = value;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.rect = new Rectangle(x, y, 190, 30);
+	}
+	
+	public void reset() {
+		this.value = defaultValue;
+	}
+	
+	public void draw(Graphics g) {
+		g.setFont(font);
+		if ("text".equals(type)) {
+			g.setColor(Color.gray);
+			g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 10, 10);
+			g.setColor(Color.black);
+			g.drawRoundRect(rect.x, rect.y, rect.width, rect.height, 10, 10);
+			g.drawString(label + ": " + value.toString(), rect.x + 5, rect.y + 20);
+		}
+		else if ("toggle".equals(type)) {
+			g.setColor((boolean) value ? Color.green : Color.red);
+			g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 10, 10);
+			g.setColor(Color.black);
+			g.drawRoundRect(rect.x, rect.y, rect.width, rect.height, 10, 10);
+			g.drawString(label, rect.x + 5, rect.y + 20);
+		}
+	}
+	
+	public boolean eventHandle(MouseEvent e) {
+		if (rect.contains(e.getPoint())) {
+			if ("toggle".equals(type)) {
+				value = !(boolean) value;
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- QuantumSketchbook/src/Sketchbook.java
+++ QuantumSketchbook/src/Sketchbook.java
@@ -7,7 +7,17 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Sketchbook {
-	public Sketchbook() {
-		
+	private String filePath;
+	private String name;
+	private BufferedImage image;
+	private Rectangle box;
+	private int modifications;
+	
+	public Sketchbook(Property f, int x, int y, int width, int height) {
+		// this.filePath = f.value;
+		this.name = new File(filePath).getName().split("\\.")[0];
+		this.box = new Rectangle(x, y, width, height);
+		this.modifications = 0;
+		// copyToTemp();
 	}
 }
