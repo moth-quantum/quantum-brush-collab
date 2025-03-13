d20621df6abcee9db07a26606a2fbc7cdea3abde
Joao <joao@mothquantum.com>
2025-03-13 10:27:59 +0100

    Changed clone to act on the SVD values

--- effects/Clone.py
+++ effects/Clone.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import numpy as np
-from qiskit.circuit.library import RXGate, RZGate,XGate,ZGate,IGate
+from qiskit.circuit.library import RXGate, RZGate,XGate,ZGate,IGate,StatePreparation
 from qiskit.circuit.library import UnitaryGate
 from qiskit.pulse import num_qubits
 from qiskit.circuit.library import RealAmplitudes
@@ -16,6 +16,17 @@ from utils import *
 #List of effect-specific requirements
 REQUIREMENTS = ["Image","Strength"]
 
+def svd(matrix=None,U=None,S=None,Vt=None):
+    if U is not None:
+        S_matrix = np.diag(S)  # Convert singular values into a diagonal matrix
+        mat = U @ S_matrix @ Vt
+        return mat
+
+    """Compute the Ordered Singular Value Decomposition (SVD) of a matrix."""
+    U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
+    sorted_indices = np.argsort(S)[::-1]  # Sort singular values in descending order
+    return U[:, sorted_indices], S[sorted_indices], Vt[sorted_indices, :]
+
 def U(m,n):
     ret = IGate().to_matrix()
     if m == 1:
@@ -29,52 +40,57 @@ def U(m,n):
 
     return ret
 
-def asym_cloning(n_steps):
-    n_qubits = 2 * n_steps + 1
+def prep(s0,s1): #s0 is the final state and s1 is the initial state
+    assert s0**2 + s1**2 + s0*s1 - s0 -s1 <= 0, "Coefs but satisfy the ellipse inequality"
+    return StatePreparation([np.sqrt((s0 + s1) / 2), np.sqrt((1 - s0) / 2), 0, np.sqrt((1 - s1) / 2)])
+
+
+def ua_cloning(n_steps,ang):
+    n_qubits = 2 * n_steps - 1
     qc = QuantumCircuit(n_qubits)
 
-    #Rotate the first qubit to encode the image
-    qc.x(0)
+    # Rotate the first qubit to encode the image
+    qc.ry(ang,0)
 
-    #Creating the bell states
-    for i in range(1,n_qubits,2):
-        qc.h(i)
-        qc.cx(i,i+1)
+    PG = prep(0.9,0.1)
 
-    w = np.array([1 / 2, 1 / 2, 1 / 2, 1 / 2]) #w00 w01 w10 w11
+    # Creating the bell states
+    for i in range(1, n_qubits, 2):
+        qc.append(PG, [i, i + 1])
 
-    w[-1] *= -1 #Need to do this to use the SparsePauliOp which does not support iY
-    op = SparsePauliOp(["III", "ZZI", "XXI", "YYI"], w).to_operator()
-    mat = SparsePauliOp(["III", "ZZI", "XXI", "YYI"], w).to_matrix()
-
-    ret = [1/i for i in range(1,n_qubits+1)]
-    return ret
     ps = []
-    for i in range(0,n_qubits,2):
-        qc.append(op, [i, i+1,i+2])
+    for i in range(0, n_qubits - 1, 2):
+        qc.cx(i, i + 2)
+        qc.cx(i, i + 1)
+        qc.cx(i + 2, i)
+        qc.cx(i + 1, i)
 
         idd = ["I"] * n_qubits
-        idd[i]="Z"
+        idd[i] = "Z"
         ps.append("".join(idd))
 
-    qc.measure_all()
+    idd = ["I"] * n_qubits
+    idd[-1] = "Z"
+    ps.append("".join(idd))
 
-    simulator = AerSimulator()
-    svec = simulator.run(qc, shots=1000).result().get_statevector()
+    svec = Statevector(qc)
 
     exp = []
-    for op in ps:
+    for s in ps:
+        op = Pauli(s)
         exp.append(svec.expectation_value(op))
 
-    return exp
+    exp = np.array(exp)
 
-def us_cloning(n_steps):
+    return np.arccos(exp)
+
+def us_cloning(n_steps,ang):
     n_qubits = 2 * n_steps - 1
     print(n_qubits)
     qc = QuantumCircuit(n_qubits)
 
     #Rotate the first qubit to encode the image
-    qc.x(0)
+    qc.ry(ang,0)
 
     t1 = - np.arccos(np.sqrt(0.5-1/2/np.sqrt(5)))
     t2 = np.arccos((np.sqrt(5)-1)/2/np.sqrt(3))
@@ -110,14 +126,8 @@ def us_cloning(n_steps):
         op = Pauli(s)
         exp.append(svec.expectation_value(op))
 
-
-    for i in range(n_qubits):
-        idd = ["I"] * n_qubits
-        idd[i] = "Z"
-        op = Pauli("".join(idd))
-    print(-np.array(exp)/2 )
-
-    return -np.array(exp)/2
+    exp = np.array(exp)
+    return np.arccos(exp)
 
 
 class Clone(BaseEffect):
@@ -136,18 +146,38 @@ class Clone(BaseEffect):
         self.radius = int(self.parameters["Radius"])
 
     def apply(self):
-        alphas = us_cloning(len(self.points))
-        #alphas /= max(alphas)
-        x0,y0 = self.points[0]
         self.new_image = self.image + 0.
-        copy = self.image[x0 - self.radius:x0 + self.radius + 1, y0 - self.radius:y0 + self.radius + 1]
 
-        for p in range(1,len(self.points)):
-            x,y = self.points[p]
-            sx = slice(x-self.radius,x+self.radius+1)
-            sy = slice(y-self.radius,y+self.radius+1)
-            paste = self.image[sx,sy]
-            self.new_image[sx,sy] = alphas[p] * copy + (1-alphas[p]) * paste
+        for i in range(3):
+            x0, y0 = self.points[0]
+            sx = slice(x0 - self.radius, x0 + self.radius + 1)
+            sy = slice(y0 - self.radius, y0 + self.radius + 1)
+
+            copy = self.image[sx,sy,i]
+            u,s,v = svd(copy)
+            ms = np.mean(s)
+            ss = np.std(s)
+
+            angles = np.pi/(1+np.exp(-(s-ms)/ss))
+            new_angles = []
+            for j,a in enumerate(angles):
+                if j <3 :
+                    new_angle = ua_cloning(len(self.points),a)
+                    print(new_angle,a)
+                    new_angles.append(new_angle)
+                else:
+                    new_angles.append([a]*len(self.points))
+
+            new_angles = np.array(new_angles)
+
+            new_s = ms - ss * np.log(np.pi/new_angles -1)
+
+            for p in range(len(self.points)):
+                x,y = self.points[p]
+                sx = slice(x-self.radius,x+self.radius+1)
+                sy = slice(y-self.radius,y+self.radius+1)
+
+                self.new_image[sx,sy,i] = svd(U=u, S=new_s[:,p], Vt= v)
 
         return self.new_image
 
