3875407f8fc6ff3e3670c6a9e7a5dd0cd76126f4
Joao <joao@mothquantum.com>
2025-03-18 13:04:22 +0100

    Added instructions

--- effects/Basic.py
+++ effects/Basic.py
@@ -8,7 +8,14 @@ from effects.BaseEffect import BaseEffect
 REQUIREMENTS = ["Image", "Color"]
 
 class Basic(BaseEffect):
+    '''
+    The name of the class must be the same as the name of the file!
+    '''
     def __init__(self, job_id=None):
+        '''
+        Always initialize like thid, don't change
+        :param job_id:
+        '''
         super().__init__()
         self.label = "Quantum Blur"
         self.requirements = REQUIREMENTS
@@ -16,12 +23,21 @@ class Basic(BaseEffect):
             self.run_job(job_id)
 
     def build(self):
+        '''
+        Here we will read self.parameters and initialize the variables.
+        Any used parameter here should be found in the REQUIREMENTS above
+        :return:
+        '''
         # TODO: Check if everything is in the correct format
         color = pygame.Color(self.parameters["Color"])
         self.color = np.array([color.r, color.g, color.b])
         self.image = np.array(self.parameters["Image"])
 
     def apply(self):
+        '''
+        In this function we will create self.new_image which wil be the updated image after the effect.
+        :return: The new updated image
+        '''
         self.image[:, :] = self.color
         self.new_image = self.image
 
@@ -29,7 +45,9 @@ class Basic(BaseEffect):
 
 
 if __name__ == "__main__":
-
+    '''
+    Always use this format but replace the name of the effect below
+    '''
     # Ensure at least one argument is passed
     if len(sys.argv) < 2:
         print("Please provide an ID as a command-line argument.")
